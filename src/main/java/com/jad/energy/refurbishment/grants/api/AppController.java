fc193c2e7afce3c8f9f868eff342c3c270af5598
Zoubair BAKKALI <zoubair.bakkali@laposte.net>
2018-02-27 00:04:57 +0100

    add Swagger specification generation

--- src/main/java/com/jad/energy/refurbishment/grants/api/AppController.java
+++ src/main/java/com/jad/energy/refurbishment/grants/api/AppController.java
@@ -1,19 +1,20 @@
 package com.jad.energy.refurbishment.grants.api;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
 public class AppController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     String home() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(AppController.class, args);
     }
 }
